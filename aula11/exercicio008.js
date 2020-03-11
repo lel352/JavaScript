a9ea894ea6e133883350fed76d86099921619d97
Leandro Sartor <leandrosartorp@gmail.com>
2020-03-11 10:51:36 -0300

    Aula 11 e exercícios

---
+++ aula11/exercicio008.js
@@ -0,0 +1,6 @@
+console.log('O Console funcionou corretamente!')
+var velocidade = 89
+console.log(`A velocidade do seu carro é ${velocidade} km/s`)
+if (velocidade >= 60) //Condição Simples
+    console.log('Você ultrapassou a velocidade permitida! Multado !!')    
+console.log('Dirija sempre usando cinto de Segurança')
