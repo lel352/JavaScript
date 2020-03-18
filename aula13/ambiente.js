c61974e1668ba5b801ddfd95c5870518753cd38f
Leandro Sartor <leandrosartorp@gmail.com>
2020-03-18 15:16:47 -0300

    aula 13 e 14 - Estrutura de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 1
+while (contador <= 6) { //teste lógico no inicio 
+    console.log(`Passo ${contador}`)
+    contador++
+}
+console.log()
+
+contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6) //teste lógico no Final
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,11 @@
+/*var contador = 1
+while (contador <= 5){ //teste lógico no inicio 
+    console.log(contador)
+    contador++
+}*/
+// ---------------------------------
+console.log('Vai começar')
+for(var c = 1;c <= 5;c++){
+    console.log(c)
+}
+console.log('Fim!')
